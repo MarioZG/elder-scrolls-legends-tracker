89de7c6e26fa17fda7a11ec75016ec15be1acdd4
MarioZG <MarioZG@users.noreply.github.com>
2016-11-21 12:34:47 -0300

    changed edit deck command ro relaycommand

--- ESLTracker/ViewModels/Decks/EditDeckViewModel.cs
+++ ESLTracker/ViewModels/Decks/EditDeckViewModel.cs
@@ -22,5 +22,41 @@ namespace ESLTracker.ViewModels.Decks
 
         public MainWindowViewModel mainWindowViewModel { get; set; }
 
+        //command for add deck button 
+        public RelayCommand CommandButtonSave
+        {
+            get {
+                return new RelayCommand(
+                    new Action<object>(CommandButtonSaveExecute), 
+                    new Func<object, bool>(CommandButtonSaveCanExecute)
+                    );
+            }
+        }
+
+
+        public void CommandButtonSaveExecute(object parameter)
+        {
+            object[] args = parameter as object[];
+            EditDeckViewModel model = args[0] as EditDeckViewModel;
+            DeckClassSelectorViewModel selectedClassModel = args[1] as DeckClassSelectorViewModel;
+            if ((model != null)
+                && (selectedClassModel != null)
+                && selectedClassModel.SelectedClass.HasValue)
+            {
+                model.Deck.Class = selectedClassModel.SelectedClass.Value;
+                model.Deck.Attributes.AddRange(Utils.ClassAttributesHelper.Classes[model.Deck.Class]);
+                DataModel.Tracker.Instance.Decks.Add(model.Deck);
+                Utils.FileManager.SaveDatabase();
+                model.Deck = new Deck();
+                model.mainWindowViewModel.DeckEditVisible = false;
+            }
+        }
+
+        public bool CommandButtonSaveCanExecute(object parameter)
+        {
+            return true;
+        }
+
+
     }
 }
