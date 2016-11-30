338f3ab77dbf2b1f10496d4a67d5a36642a5c642
MarioZG <MarioZG@users.noreply.github.com>
2016-11-30 09:38:40 -0600

    serialise arena rank as int value

--- ESLTracker/DataModel/Enums/ArenaRank.cs
+++ ESLTracker/DataModel/Enums/ArenaRank.cs
@@ -1,15 +1,26 @@
-﻿namespace ESLTracker.DataModel.Enums
+﻿using System.Xml.Serialization;
+
+namespace ESLTracker.DataModel.Enums
 {
     public enum ArenaRank
     {
+        [XmlEnum("1")]
         Level1 = 1,
+        [XmlEnum("2")]
         Level2,
+        [XmlEnum("3")]
         Level3,
+        [XmlEnum("4")]
         Gladiator,
+        [XmlEnum("5")]
         Level5,
+        [XmlEnum("6")]
         Level6,
+        [XmlEnum("7")]
         Level7,
+        [XmlEnum("8")]
         Brawler,
+        [XmlEnum("9")]
         Level9
     }
 }
