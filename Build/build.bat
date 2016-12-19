b6c269260ca99d43066e8508197c2500693cc924
MarioZG <MarioZG@users.noreply.github.com>
2016-12-19 19:02:57 +0100

    build.bat do not call cleanup when main msbuild fails

--- Build/build.bat
+++ Build/build.bat
@@ -1,3 +1,7 @@
 "C:\Program Files (x86)\MSBuild\14.0\bin\msbuild" build.xml /fl
+echo off
+if %errorlevel% gtr 0 goto error
+echo on
 "C:\Program Files (x86)\MSBuild\14.0\bin\msbuild" build.xml /target:CleanUp
+:error
 pause
