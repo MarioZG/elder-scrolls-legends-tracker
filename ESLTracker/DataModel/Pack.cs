8805059c586bddccc0430f0ed7642c2cde2c37da
MarioZG <MarioZG@users.noreply.github.com>
2016-12-27 15:58:00 +0100

    pack gems value

--- ESLTracker/DataModel/Pack.cs
+++ ESLTracker/DataModel/Pack.cs
@@ -4,26 +4,38 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ESLTracker.ViewModels;
 
 namespace ESLTracker.DataModel
 {
-    public class Pack
+    public class Pack : ViewModelBase
     {
         public ObservableCollection<CardInstance> Cards { get; set; } = new ObservableCollection<CardInstance>()
-             { new CardInstance(Utils.CardsDatabase.Default.FindCardByName("Adoring Fan")),new CardInstance(), new CardInstance(), new CardInstance(), new CardInstance(), new CardInstance()};
+             { new CardInstance(), new CardInstance(), new CardInstance(), new CardInstance(), new CardInstance(), new CardInstance()};
 
         public DateTime DateOpened { get; set; }
 
-        public int DustValue
+        public int SoulGemsValue
         {
             get
             {
+                var notNullCards = Cards.Where(c => c.Card != null);
                 return
-                    Cards.Where(c => c.Card.Rarity == Enums.CardRarity.Common).Count() * 5
-                    + Cards.Where(c => c.Card.Rarity == Enums.CardRarity.Rare).Count() * 20
-                    + Cards.Where(c => c.Card.Rarity == Enums.CardRarity.Epic).Count() * 100
-                    + Cards.Where(c => c.Card.Rarity == Enums.CardRarity.Legendary).Count() * 400;
+                    notNullCards.Where(c => c.Card.Rarity == Enums.CardRarity.Common).Count() * 5
+                    + notNullCards.Where(c => c.Card.Rarity == Enums.CardRarity.Rare).Count() * 20
+                    + notNullCards.Where(c => c.Card.Rarity == Enums.CardRarity.Epic).Count() * 100
+                    + notNullCards.Where(c => c.Card.Rarity == Enums.CardRarity.Legendary).Count() * 400;
             }
+        }
+
+        public Pack()
+        {
+            Cards.CollectionChanged += Cards_CollectionChanged;
+        }
+
+        private void Cards_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            RaisePropertyChangedEvent(nameof(SoulGemsValue));
         }
     }
 }
