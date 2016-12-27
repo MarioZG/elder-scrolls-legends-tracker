1ab960d4948f72f39dc4ae5cd361024572e6e3cb
MarioZG <MarioZG@users.noreply.github.com>
2016-12-27 10:35:52 +0100

    card database update

--- ESLTrackerTests/Utils/CardsDatabaseTests.cs
+++ ESLTrackerTests/Utils/CardsDatabaseTests.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ESLTracker.DataModel;
 using ESLTracker.DataModel.Enums;
+using System.Collections.ObjectModel;
 
 namespace ESLTracker.Utils.Tests
 {
@@ -48,11 +49,25 @@ namespace ESLTracker.Utils.Tests
             Assert.AreEqual(true, card.IsUnique);
             Assert.AreEqual(CardType.Creature, card.Type);
             Assert.AreEqual(2, card.Cost);
-            CollectionAssert.AreEqual(new DeckAttributes() {  DeckAttribute.Intelligence , DeckAttribute.Willpower}, card.Attributes);
+            CollectionAssert.AreEqual(new DeckAttributes() { DeckAttribute.Intelligence, DeckAttribute.Willpower }, card.Attributes);
             Assert.AreEqual(3, card.Attack);
             Assert.AreEqual(1, card.Health);
             Assert.AreEqual("highelf", card.Race);
             Assert.AreEqual("Card text", card.Text);
+        }
+
+        [TestMethod()]
+        public void PopulateCollectionTest001()
+        {
+            Pack pack = new Pack();
+            ObservableCollection<string> names = new ObservableCollection<string>( 
+               new string[] { "Adoring Fan", "random name", String.Empty, String.Empty, String.Empty, String.Empty }
+            );
+            CardsDatabase.Default.PopulateCollection(names, pack.Cards);
+
+            Assert.AreEqual(6, pack.Cards.Count);
+            Assert.AreEqual("Adoring Fan", pack.Cards[0].Card.Name);
+            Assert.AreEqual(Card.Unknown, pack.Cards[1].Card);
         }
     }
 }
