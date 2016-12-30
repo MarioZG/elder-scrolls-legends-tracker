2fc20befb23c16975676ef03a35a3dfb1cac8088
MarioZG <MarioZG@users.noreply.github.com>
2016-12-30 20:24:13 +0100

    updated small images, test to ensure images for all cards are present

--- ESLTrackerTests/Utils/CardsDatabaseTests.cs
+++ ESLTrackerTests/Utils/CardsDatabaseTests.cs
@@ -8,13 +8,22 @@ using System.Threading.Tasks;
 using ESLTracker.DataModel;
 using ESLTracker.DataModel.Enums;
 using System.Collections.ObjectModel;
+using ESLTrackerTests;
+using System.Reflection;
 
 namespace ESLTracker.Utils.Tests
 {
     [TestClass]
     [DeploymentItem("./Resources/cards.json", "./Resources/")]
-    public class CardsDatabaseTests
+    public class CardsDatabaseTests : BaseTest
     {
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            string s = System.IO.Packaging.PackUriHelper.UriSchemePack;
+            System.Windows.Application.ResourceAssembly = Assembly.GetAssembly(typeof(App));
+        }
+
         [TestMethod]
         public void FindCardByNameTest001_UnknownCard()
         {
@@ -28,6 +37,21 @@ namespace ESLTracker.Utils.Tests
         public void LoadCardsDatabaseTest()
         {
             Assert.IsNotNull(CardsDatabase.Default.Cards);
+
+        }
+
+        [TestMethod]
+        public void CardsDatabaseTest_EnsureAllCardsHaveImages()
+        {
+            foreach(Card card in CardsDatabase.Default.Cards)
+            {
+                Assert.IsNotNull(new CardInstance(card).BackgroundColor, card.Name);
+                Assert.IsNotNull(new CardInstance(card).ForegroundColor, card.Name);
+                Assert.IsNotNull(new CardInstance(card).RarityColor, card.Name);
+
+                Uri imageUri = new Uri(card.ImageName, UriKind.RelativeOrAbsolute);
+                Assert.IsTrue(ResourcesHelper.ResourceExists(imageUri), card.Name);
+            }
 
         }
 
