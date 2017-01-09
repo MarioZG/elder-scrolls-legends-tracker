fba6211a25bd6a362481285d96e8fefac052ebd8
MarioZG <MarioZG@users.noreply.github.com>
2017-01-09 15:43:06 +0100

    do not show tooltip on select card when no card selected

---
+++ ESLTracker/Utils/Converters/ObjectToBool.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESLTracker.Utils.Converters
+{
+    public class ObjectToBool : InvertibleConverter<bool, object>
+    {
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (converter == null)
+            {
+                converter = new ObjectToBool();
+            }
+            return converter;
+        }
+
+        protected override bool ReturnWhenFalse
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        protected override bool ReturnWhenTrue
+        {
+            get
+            {
+               return true;
+            }
+        }
+
+        protected override bool Condition(object value)
+        {
+            return value != null;
+        }
+    }
+}
