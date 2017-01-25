23e318c132b8fa84fdc80d79095b52e7e6b54f23
MarioZG <MarioZG@users.noreply.github.com>
2017-01-25 20:32:45 +0000

    hide deck cards breakdown when no cards

---
+++ ESLTracker/Utils/Converters/CollectionCountToVisibility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESLTracker.Utils.Converters
+{
+    class CollectionCountToVisibility : ToVisibilityConverter<IList>
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (converter == null)
+            {
+                converter = new CollectionCountToVisibility();
+            }
+            return converter;
+        }
+
+        protected override bool Condition(object value)
+        {
+            return ((IList)value).Count > 0;
+        }
+    }
+}
