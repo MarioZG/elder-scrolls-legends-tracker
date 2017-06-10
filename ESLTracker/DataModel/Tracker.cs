73b872afe3368bfd437d47e8b87ec58d3caf7115
MarioZG <MarioZG@users.noreply.github.com>
2017-06-10 07:51:13 +0100

    increased file verion

--- ESLTracker/DataModel/Tracker.cs
+++ ESLTracker/DataModel/Tracker.cs
@@ -39,7 +39,7 @@ namespace ESLTracker.DataModel
   
 
         [XmlIgnore]
-        public static SerializableVersion CurrentFileVersion = new SerializableVersion(2, 2);
+        public static SerializableVersion CurrentFileVersion = new SerializableVersion(2, 3);
 
         // binding!!!
 
---
+++ ESLTracker/Utils/FileUpdaters/Update_2_2_To_2_3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using ESLTracker.DataModel;
+
+namespace ESLTracker.Utils.FileUpdaters
+{
+    [SerializableVersion(2, 2)]
+    public class Update_2_2_To_2_3 : UpdateBase
+    {
+        public override SerializableVersion TargetVersion { get; } = new SerializableVersion(2, 3);
+
+        protected override void VersionSpecificUpdateFile(XmlDocument doc, Tracker tracker)
+        {
+            //do nothing - just mark for new gametype values
+        }
+
+    }
+}
